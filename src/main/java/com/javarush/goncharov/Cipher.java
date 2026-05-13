0ae4c2aefe3e7f75d517db79177684862d0fd282
Avg0n <avgon@morra.su>
2026-05-13 22:19:17 +0300

    Change variable name

--- src/main/java/com/javarush/goncharov/Cipher.java
+++ src/main/java/com/javarush/goncharov/Cipher.java
@@ -1,14 +1,15 @@
 package com.javarush.goncharov;
 
 public class Cipher {
-    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+    private static final char[] alphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
-    public Cipher(char[] ALPHABET) {
-        this.ALPHABET = ALPHABET;
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
     }
-    public String encrypt(String text, int shift) {
+    public String encrypt(String pureText, int shift) {
         // Логика шифрования
+
     }
     public String decrypt(String encryptedText, int shift) {
         // Логика расшифровки
